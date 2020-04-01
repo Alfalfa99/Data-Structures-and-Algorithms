c2d4b988fd4a79433208909d75a4c7308e994699
alfalfa99 <1542342959@qq.com>
2020-04-01 23:07:58 +0800

    day4

---
+++ LeetCode/src/LeetCode28.java
@@ -0,0 +1,9 @@
+public class LeetCode28 {
+    public static void main(String[] args) {
+
+    }
+
+    public int strStr(String haystack, String needle) {
+        return haystack.indexOf(needle);
+    }
+}
