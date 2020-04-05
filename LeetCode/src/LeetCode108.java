1d08fcc23ccfcb7ee56cbae6db13f70a3d7c1009
alfalfa99 <1542342959@qq.com>
2020-04-06 01:35:39 +0800

    day8 update

--- LeetCode/src/LeetCode108.java
+++ LeetCode/src/LeetCode108.java
@@ -11,9 +11,6 @@ public class LeetCode108 {
 //        p.left.right = new TreeNode(3);
         p.right.left = new TreeNode(15);
         p.right.right = new TreeNode(7);
-        for (List<Integer> list : levelOrderBottom(p)) {
-            System.out.println(list);
-        }
     }
 
     public static class TreeNode {
@@ -26,54 +23,24 @@ public class LeetCode108 {
         }
     }
 
-
-    public static List<List<Integer>> levelOrderBottom(TreeNode root) {
-        LinkedList<List<Integer>> list = new LinkedList<>();
-        if (root == null) {
-            return list;
-        }
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.offer(root);
-        while (!queue.isEmpty()) {
-            LinkedList<Integer> linkedList = new LinkedList<>();
-            int size = queue.size();
-            for (int i = 0; i < size; i++) {
-                TreeNode treeNode = queue.poll();
-                linkedList.offer(treeNode.val);
-                if (treeNode.left != null) {
-                    queue.offer(treeNode.left);
-                }
-                if (treeNode.right != null) {
-                    queue.offer(treeNode.right);
-                }
-            }
-            list.offerFirst(linkedList);
-        }
-        return list;
-    }
+    int nums[];
 
     public TreeNode sortedArrayToBST(int[] nums) {
         if (nums.length == 0) {
             return null;
         }
-        if (nums.length == 1) {
-            return new TreeNode(nums[0]);
+        this.nums = nums;
+        return degui(0, nums.length - 1);
+    }
+
+    public TreeNode degui(int left, int right) {
+        if (left > right){
+            return null;
         }
-        int mid, left, right;
-        right = nums.length - 1;
-        if (nums.length % 2 == 0) {
-            mid = nums[nums.length / 2] > nums[nums.length / 2 + 10] ? nums.length / 2 : nums.length / 2 + 1;
-        } else {
-            mid = nums.length / 2;
-        }
-        left = mid - 1;
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.offer(new TreeNode(nums[mid]));
-        while (!queue.isEmpty()){
-            int size = queue.size();
-            for (int i = 0; i < ; i++) {
-                
-            }
-        }
+        int mid = (left + right) / 2;
+        TreeNode treeNode = new TreeNode(this.nums[mid]);
+        treeNode.left = degui(left,mid-1);
+        treeNode.right = degui(mid+1,right);
+        return treeNode;
     }
 }
