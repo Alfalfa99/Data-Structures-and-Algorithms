7dd2e49971fb6ab9328c02c78585a630b939d252
alfalfa99 <1542342959@qq.com>
2020-04-12 00:19:48 +0800

    day14 update

--- LeetCode/src/LeetCode3.java
+++ LeetCode/src/LeetCode3.java
@@ -7,16 +7,16 @@ public class LeetCode3 {
     }
 
     public static int lengthOfLongestSubstring(String s) {
-        int n = s.length(), ans = 0;
+        int n = s.length(), max = 0;
         Map<Character, Integer> map = new HashMap<>();
         for (int end = 0, start = 0; end < n; end++) {
             char alpha = s.charAt(end);
             if (map.containsKey(alpha)) {
                 start = Math.max(map.get(alpha), start);
             }
-            ans = Math.max(ans, end - start + 1);
+            max = Math.max(max, end - start + 1);
             map.put(s.charAt(end), end + 1);
         }
-        return ans;
+        return max;
     }
 }
