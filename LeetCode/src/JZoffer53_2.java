843cf4be30bb5c037b3e65fc66894c16ee63ead7
alfalfa99 <1542342959@qq.com>
2020-05-29 16:16:19 +0800

    day62

---
+++ LeetCode/src/JZoffer53_2.java
@@ -0,0 +1,19 @@
+public class JZoffer53_2 {
+    public static void main(String[] args) {
+        int[] nums = new int[]{0, 1, 2, 3, 4, 5, 6, 7, 9};
+        System.out.println(missingNumber(nums));
+    }
+
+    public static int missingNumber(int[] nums) {
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] == mid) {
+                left = mid + 1;
+            } else if (nums[mid] > mid) {
+                right = mid - 1;
+            }
+        }
+        return left;
+    }
+}
