6d5832640eb3014738e007e9054ee4cdaf17196f
alfalfa99 <1542342959@qq.com>
2020-06-08 23:24:20 +0800

    day72

---
+++ LeetCode/src/JZoffer68_1.java
@@ -0,0 +1,13 @@
+public class JZoffer68_1 {
+
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root.val < p.val && root.val < q.val) {
+            return lowestCommonAncestor(root.right, p, q);
+        }
+        if (root.val > p.val && root.val > q.val) {
+            return lowestCommonAncestor(root.left, p, q);
+        }
+        return root;
+    }
+}
