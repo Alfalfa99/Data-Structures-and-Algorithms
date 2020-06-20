d6ee55b3df60d33af0c6abc8bc585f1c9ee4c204
alfalfa99 <1542342959@qq.com>
2020-06-20 14:58:11 +0800

    day84

---
+++ LeetCode/src/LeetCode1208.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class LeetCode1208 {
+    public static void main(String[] args) {
+        System.out.println(equalSubstring("abcd", "bcdf", 3));
+    }
+
+    public static int equalSubstring(String s, String t, int maxCost) {
+        int left = 0, right = 0;
+        int nowCost = 0;
+        while(right < s.length()) {
+            int cost = Math.abs(s.charAt(right) - t.charAt(right));
+            nowCost += cost;
+            if (nowCost > maxCost) {
+                nowCost -= Math.abs(s.charAt(left) - t.charAt(left));
+                left++;
+                right++;
+            }
+            else {
+                right++;
+            }
+        }
+
+        return right - left;
+    }
+}
