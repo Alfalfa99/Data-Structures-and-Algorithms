9c7bf0611f1dd7e00827cb4890a7375b3262137e
alfalfa99 <1542342959@qq.com>
2020-07-14 23:54:48 +0800

    day108

--- LeetCode/src/LeetCode/LeetCode0_Test.java
+++ LeetCode/src/LeetCode/LeetCode0_Test.java
@@ -7,27 +7,16 @@ public class LeetCode0_Test {
 
     }
 
-    public static int[] intersect(int[] nums1, int[] nums2) {
-        // 排好序的写法
-        Arrays.sort(nums1);
-        Arrays.sort(nums2);
-        // 定义i，j两个指针，k用于记录交集的索引值
-        int i = 0, j = 0, k = 0;
-        while (i < nums1.length && j < nums2.length) {
-            if(nums1[i] < nums2[j]) {
-                // 说明num1[i]较小，i右移
-                i++;
-            } else if (nums1[i] > nums2[j]) {
-                // 说明nums2[j]较小，j右移
-                j++;
-            } else {
-                // 两个元素相同，记录在k
-                nums1[k++] = nums1[i];
-                // 两个指针向前走一步
-                i++;
-                j++;
+    public int minimumTotal(List<List<Integer>> triangle) {
+        int height = triangle.size();
+        if (height == 0){
+            return 0;
+        }
+        for (int i = height-2; i >= 0 ; i--) {
+            for (int j = 0; j < triangle.get(i).size(); j++) {
+                triangle.get(i).set(j,triangle.get(i).get(j)+Math.min(triangle.get(i+1).get(j),triangle.get(i+1).get(j+1)));
             }
         }
-        return Arrays.copyOfRange(nums1, 0, k);
+        return triangle.get(0).get(0);
     }
 }
