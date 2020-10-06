444aa5ccc581ab8e3f659a2253eeaef271b9f1b6
Alfalfa99 <42247425+Alfalfa99@users.noreply.github.com>
2020-10-06 19:03:39 +0800

    day150

--- LeetCode/src/LeetCode/LeetCode0_Test.java
+++ LeetCode/src/LeetCode/LeetCode0_Test.java
@@ -6,51 +6,8 @@ import util.TreeNode;
 import java.util.*;
 
 public class LeetCode0_Test {
-    class Node {
-        public int val;
-        public Node left;
-        public Node right;
-        public Node next;
-
-        public Node() {}
-
-        public Node(int _val) {
-            val = _val;
-        }
-
-        public Node(int _val, Node _left, Node _right, Node _next) {
-            val = _val;
-            left = _left;
-            right = _right;
-            next = _next;
-        }
-    }
-
-    Deque<Node> curNodeQueue = new ArrayDeque<>();
-    public Node connect(Node root) {
-        if(root == null){
-            return root;
-        }
-        curNodeQueue.add(root);
-        int size = curNodeQueue.size();
-        while(size != 0){
-            Node curNode = null;
-            for (int i = 0; i < size; i++) {
-                Node node = curNodeQueue.pollFirst();
-                if(node.left != null){
-                    curNodeQueue.add(node.left);
-                }
-                if(node.right != null){
-                    curNodeQueue.add(node.right);
-                }
-                if(curNode != null){
-                    curNode.next = node;
-                }
-                curNode = node;
-            }
-            size = curNodeQueue.size();
-            curNode = null;
-        }
-        return root;
+    public static void main(String[] args) {
+        String s = "123asd";
+        System.out.println(s.toUpperCase());
     }
 }
