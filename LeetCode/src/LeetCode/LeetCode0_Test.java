5031dd0fb63bf5683f57257adeefcf7c82a2594d
alfalfa99 <1542342959@qq.com>
2020-10-12 00:41:36 +0800

    day151

--- LeetCode/src/LeetCode/LeetCode0_Test.java
+++ LeetCode/src/LeetCode/LeetCode0_Test.java
@@ -7,23 +7,39 @@ import java.util.*;
 
 public class LeetCode0_Test {
     public static void main(String[] args) {
+        System.out.println(canPartition(new int[]{1,2,3,4,5,6,7}));
+    }
 
-    }
-    static ListNode realHead;
-    public static ListNode reverseList(ListNode head) {
-        if(head == null){
-            return null;
+    public static boolean canPartition(int[] nums) {
+        if(nums.length == 0){
+            return false;
         }
-        dfs(head, head.next);
-        return realHead;
-    }
-    public static void dfs(ListNode pre, ListNode cur) {
-        if(cur == null){
-            return;
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
         }
-        dfs(cur, cur.next);
-        realHead = cur;
-        cur.next = pre;
-        pre.next = null;
+        if (sum % 2 != 0) {
+            return false;
+        }
+        int target = sum / 2;
+        boolean[][] dp = new boolean[nums.length][target + 1];
+        //初始化
+        if (nums[0] <= target) {
+            dp[0][nums[0]] = true;
+        }
+
+        for (int i = 1; i < nums.length; i++) {
+            for (int j = 0; j <= target; j++) {
+                dp[i][j] = dp[i - 1][j];
+                if (j == nums[i]) {
+                    dp[i][j] = true;
+                    continue;
+                }
+                if (nums[i] < j) {
+                    dp[i][j] = dp[i - 1][j - nums[i]] || dp[i - 1][j];
+                }
+            }
+        }
+        return dp[nums.length - 1][target];
     }
 }
