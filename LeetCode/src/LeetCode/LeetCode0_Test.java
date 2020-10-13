aa79ad64d847f7b8e88078e38c00e8f0c83cf740
Alfalfa99 <42247425+Alfalfa99@users.noreply.github.com>
2020-10-13 18:35:07 +0800

    day153

--- LeetCode/src/LeetCode/LeetCode0_Test.java
+++ LeetCode/src/LeetCode/LeetCode0_Test.java
@@ -1,31 +1,33 @@
 package LeetCode;
 
-import util.ListNode;
-import util.TreeNode;
 
-import java.util.*;
 
 public class LeetCode0_Test {
     public static void main(String[] args) {
-        System.out.println(coinChange(new int[]{2, 5, 10, 1}, 27));
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        System.out.println(swapPairs(head));
     }
 
-    public static int coinChange(int[] coins, int amount) {
-        int len = coins.length;
-        if (len == 0){
-            return -1;
+    static ListNode pre = null, virtual_head = new ListNode();
+
+    public static ListNode swapPairs(ListNode head) {
+        if ((head == null) || (head.next == null)) {
+            return head;
         }
-        int[] dp = new int[amount+1];
-        Arrays.fill(dp,amount+1);
-        dp[0] = 0;
-        for(int coin : coins){
-            for(int i = coin; i <= amount; i++){
-                dp[i] = Math.min(dp[i],dp[i-coin]+1);
-            }
-        }
-        if(dp[amount] == amount+1){
-            dp[amount] = -1;
-        }
-        return dp[amount];
+
+        // Nodes to be swapped
+        ListNode firstNode = head;
+        ListNode secondNode = head.next;
+
+        // Swapping
+        firstNode.next  = swapPairs(secondNode.next);
+        secondNode.next = firstNode;
+
+        // Now the head is the second node
+        return secondNode;
     }
+
 }
